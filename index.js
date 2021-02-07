d9e86d9ee5159ed8ef2ed08f5da5f27ae524630a
doodlezucc <electerminator@gmail.com>
2021-02-07 03:49:22 +0100

    don't need no javascript (yet)

--- index.js
+++
@@ -1,6 +0,0 @@
-for (let app of document.getElementById("apps").children) {
-    console.log(app);
-    app.onclick = function(e) {
-        window.location.href = this.getAttribute("href");
-    }
-}
