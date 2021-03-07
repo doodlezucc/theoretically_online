0f993a8d118aa7bf758bb63c079d1e715c4e9716
doodlezucc <electerminator@gmail.com>
2021-03-07 23:53:55 +0100

    made title less attention grabbing

--- web/dart/title.dart
+++ web/dart/title.dart
@@ -10,27 +10,31 @@ void initializeTitle(String left, String right) {
     "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif",
     'Montserrat',
   ];
-  _createSide(left, _container.children[0], [
+  _createSide(left, _container.children[0], 0, [
     'Arvo',
     ...shared,
   ]);
-  _createSide(right, _container.children[1], [
-    'Arial',
+  _createSide(right, _container.children[1], 1, [
     ...shared,
+    'Arial',
+    'Montserrat',
   ]);
 }
 
-void _createSide(String s, HtmlElement c, List<String> fonts) async {
-  var rnd = Random();
-  for (var font in fonts..shuffle(rnd)) {
-    c.append(HeadingElement.h1()
-      ..text = s
-      ..style.fontFamily = font);
+void _createSide(String s, HtmlElement c, int off, List<String> fonts) async {
+  var repeat = 2;
+  for (var i = 0; i < repeat; i++) {
+    for (var font in fonts) {
+      c.append(HeadingElement.h1()
+        ..text = s
+        ..style.fontFamily = font);
+    }
   }
 
-  while (true) {
-    var i = rnd.nextInt(fonts.length);
-    c.style.top = 'calc(-$i * var(--height))';
-    await Future.delayed(Duration(milliseconds: 1000 + rnd.nextInt(3000)));
-  }
+  var rnd = Random();
+  var i = 1 - off + rnd.nextInt(repeat * fonts.length - 1);
+  c.style.top = 'calc(-$i * var(--height))';
+
+  await Future.delayed(Duration(milliseconds: 1000 + rnd.nextInt(1000)));
+  c.style.top = 'calc(${-off * (repeat * fonts.length - 1)} * var(--height))';
 }
