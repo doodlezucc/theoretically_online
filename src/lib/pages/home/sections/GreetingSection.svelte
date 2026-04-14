67ecd241bb7345c85f477ebb8704d5e400f1b25a
doodlezucc <electerminator@gmail.com>
2026-04-15 00:03:05 +0200

    Add name card to greeting section

--- src/lib/pages/home/sections/GreetingSection.svelte
+++ src/lib/pages/home/sections/GreetingSection.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { asset } from '$app/paths';
 	import { backgroundStopMotion } from '$lib/common/background-stop-motion';
+	import NameCard from './introduction/NameCard.svelte';
 </script>
 
 <section
@@ -10,10 +11,14 @@
 >
 	<h2>Hello!</h2>
 	<p>I make web apps and stuff.</p>
+
+	<div class="name-card">
+		<NameCard />
+	</div>
 </section>
 
 <style lang="scss">
-	@use '$lib/style/constants.scss' as *;
+	$dz-background: #eb6363;
 
 	section {
 		background-image: var(--texture);
@@ -21,10 +26,29 @@
 
 		text-align: center;
 		justify-content: center;
+	}
 
-		> p {
-			padding-bottom: 0;
-			margin: 0;
+	p {
+		margin: 0;
+	}
+
+	.name-card {
+		margin: 40px 0;
+
+		cursor: default;
+		scale: 1;
+		transition: scale 0.2s cubic-bezier(0.175, 0.885, 0.32, 1.275); // Overshoots
+
+		&:hover {
+			scale: 1.1;
+		}
+	}
+
+	@media (min-width: 1500px) {
+		.name-card {
+			position: absolute;
+			translate: 120% 0%;
+			rotate: -2deg;
 		}
 	}
 </style>
